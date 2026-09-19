6598b4dd704dad7faf515480b0e46836cef82468
govardhangantla <govardhanrajugantla@gmail.com>
2026-09-19 23:48:31 +0530

    Day-23 javascript introduction

---
+++ Day-23-JAVASCRIPT-Introduction/day-01.js
@@ -0,0 +1,98 @@
+// ========================================
+// JavaScript Journey - Day 1
+// Introduction to JavaScript
+// ========================================
+
+
+// 1. First JavaScript Program
+
+console.log("Hello World!");
+
+
+// 2. Personal Information
+
+console.log("My name is Govardhan");
+console.log("I am a CSE student");
+console.log("I am learning JavaScript");
+console.log("I love cricket");
+console.log("I want to become a Software Engineer");
+
+
+// 3. Printing Text
+
+console.log("Hello");
+console.log("Welcome to JavaScript");
+
+
+// 4. Printing Numbers
+
+console.log(10);
+console.log(100);
+console.log(2026);
+
+
+// 5. Basic Addition
+
+console.log(10 + 20);
+console.log(25 + 15);
+
+
+// 6. Basic Subtraction
+
+console.log(50 - 20);
+console.log(100 - 45);
+
+
+// 7. Basic Multiplication
+
+console.log(10 * 5);
+console.log(12 * 4);
+
+
+// 8. Basic Division
+
+console.log(100 / 4);
+console.log(50 / 5);
+
+
+// 9. Different Calculations
+
+console.log(10 + 5);
+console.log(20 - 8);
+console.log(6 * 7);
+console.log(100 / 10);
+
+
+// 10. Simple Student Information
+
+console.log("===== STUDENT INFORMATION =====");
+
+console.log("Name: Govardhan");
+console.log("Branch: CSE");
+console.log("Year: 2");
+console.log("Goal: Software Engineer");
+
+
+// 11. Simple Introduction
+
+console.log("===== MY INTRODUCTION =====");
+
+console.log("Hello!");
+console.log("My name is Govardhan.");
+console.log("I am a CSE student.");
+console.log("I am starting my JavaScript journey.");
+
+
+// 12. Simple Calculation Example
+
+console.log("===== CALCULATIONS =====");
+
+console.log("Addition:", 25 + 15);
+console.log("Subtraction:", 50 - 20);
+console.log("Multiplication:", 10 * 5);
+console.log("Division:", 100 / 4);
+
+
+// ========================================
+// End of Day 1
+// ========================================
